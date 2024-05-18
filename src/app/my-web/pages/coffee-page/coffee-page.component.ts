66c001315704014d7bf9dff38263648fe101ea84
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2024-05-18 01:14:04 -0500

    rename and refactor component

---
+++ src/app/my-web/pages/coffee-page/coffee-page.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'my_web-coffee-page',
+  templateUrl: './coffee-page.component.html',
+  styleUrls: ['./coffee-page.component.css']
+})
+export class CoffeePageComponent {
+
+}
