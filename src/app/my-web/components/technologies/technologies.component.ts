20b74e864f4db630ab6b49a7aa81564f8035b68d
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2024-05-22 21:42:44 -0500

    add technologies

--- src/app/my-web/components/technologies/technologies.component.ts
+++ src/app/my-web/components/technologies/technologies.component.ts
@@ -6,11 +6,23 @@ import {Component} from '@angular/core';
   styleUrls: ['./technologies.component.css']
 })
 export class TechnologiesComponent {
+  pathImage: string = 'assets/technologies/';
+
   technologies = [
-    {name: 'TypeScript', image: 'ruta/a/imagen/typescript.png'},
-    {name: 'JavaScript', image: 'ruta/a/imagen/javascript.png'},
-    {name: 'NPM', image: 'ruta/a/imagen/npm.png'},
-    {name: 'Angular', image: 'ruta/a/imagen/angular.png'}
+    {name: 'java', image: `${this.pathImage}java.svg`},
+    {name: 'spring', image: `${this.pathImage}spring.svg`},
+    {name: 'ts', image: `${this.pathImage}ts.svg`},
+    {name: 'js', image: `${this.pathImage}js.svg`},
+    {name: 'angular', image: `${this.pathImage}angular.svg`},
+    {name: 'python', image: `${this.pathImage}python.svg`},
+    {name: 'git', image: `${this.pathImage}git.svg`},
+    {name: 'jenkins', image: `${this.pathImage}jenkins.svg`},
+    {name: 'azure', image: `${this.pathImage}azure.svg`},
+    {name: 'docker', image: `${this.pathImage}docker.svg`},
+    {name: 'kubernetes', image: `${this.pathImage}kubernetes.svg`},
+    {name: 'dynatrace', image: `${this.pathImage}dynatrace.svg`},
+    {name: 'grafana', image: `${this.pathImage}grafana.svg`},
+    {name: 'jira', image: `${this.pathImage}jira.svg`},
     // Agrega más tecnologías aquí
   ];
 }
