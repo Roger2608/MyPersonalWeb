67211fc11ad247a61846822a0fa7012cec387391
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2024-11-11 22:16:47 -0500

    modify test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {SharedModule} from "./shared/shared.module";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [SharedModule]
     }).compileComponents();
   });
 
@@ -20,12 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     // expect(app.title).toEqual('MyPersonalWeb');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyPersonalWeb app is running!');
   });
 });
